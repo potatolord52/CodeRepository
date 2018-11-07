c6b8078c5af7821ac16a98fe4ad64fbdca08860f
My Code <43992776+potatolord52@users.noreply.github.com>
2018-11-07 14:47:10 +0000

    Add files via upload

---
+++ phrase-divider.py
@@ -0,0 +1,9 @@
+sentence1=str(input("Enter the first short sentence: "))
+sentence2=str(input("Enter the second short sentence: "))
+words1=sentence1.split()
+words2=sentence2.split()
+bigSentence=words1+words2
+print(len(bigSentence))
+
+#print("\nLength of sentences is:",len(final))
+#How do I sort aphabetically ignoring caps? the sort() function takes caps into consideration and prioritizes them
