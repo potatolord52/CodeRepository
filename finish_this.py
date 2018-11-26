8ad4723ffd249e20e9704a64b566d861490e2a10
My Code <43992776+potatolord52@users.noreply.github.com>
2018-11-26 18:20:54 +0000

    Add files via upload

---
+++ finish_this.py
@@ -0,0 +1,9 @@
+def main():
+    phrase=input("Enter a sentence: ")
+    count_spaces(phrase)
+def count_spaces(enter):
+    spaces=0
+    for char in enter:
+        if char==" ":
+            spaces=spaces+1
+print(main())
