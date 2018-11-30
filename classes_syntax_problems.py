087ad63adca3649140443959e06f4693eea2512c
My Code <43992776+potatolord52@users.noreply.github.com>
2018-11-30 17:51:43 +0000

    Add files via upload

---
+++ classes_syntax_problems.py
@@ -0,0 +1,7 @@
+class Person():
+    def __init__(self,firstName="Harry"):
+        self.firstName=firstName
+    def get_firstName(self):
+        print(self.firstName)
+
+print(Person(get_firstName(self)))
